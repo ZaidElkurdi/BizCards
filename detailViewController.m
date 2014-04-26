8db6135858cc8a24751b57480824cbb13b9f7021
Aryaman Sharda <aryamansharda@s00-0000-5245-res.dynamic.ucsd.edu>
2014-04-26 09:43:49 -0700

    Detail view code (80%)

--- detailViewController.m
+++ detailViewController.m
@@ -9,7 +9,9 @@
 #import "detailViewController.h"
 
 @interface detailViewController ()
-
+{
+    UIActionSheet *phonePicker;
+}
 @end
 
 @implementation detailViewController
@@ -34,15 +36,29 @@
     self.navigationController.navigationBar.frame = CGRectMake(0, 0, 320, 40);
     self.navigationController.navigationBar.barTintColor=barColor;
     NSLog(@"Nav bar: %@", self.navigationController.navigationBar);
-    UILabel *navTitle = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 320, 40)];
-    navTitle.text = @"Aryaman Sharda";
-    navTitle.textColor = [UIColor whiteColor];
-    UIFont *navFont = [UIFont fontWithName:@"HelveticaNeue-Medium" size:28.0];
-    navTitle.font = navFont;
-    navTitle.textAlignment = NSTextAlignmentCenter;
-    [self.navigationController.navigationBar addSubview:navTitle];
     
     [self setNeedsStatusBarAppearanceUpdate];
+    
+    /* Init instance variables */
+    self.phoneNumbers = [self.cardData objectForKey:@"phoneNumbers"];
+    self.emailAddresses = [self.cardData objectForKey:@"emailAddresses"];
+    self.fullName = [self.cardData objectForKey:@"fullName"];
+    self.position = [self.cardData objectForKey:@"position"];
+    self.uniqueID = [self.cardData objectForKey:@"uniqueID"];
+    self.linkedInID = [self.cardData objectForKey:@"linkedInID"];
+    
+//    NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
+//    
+//    if([prefs objectForKey:self.uniqueID] != NULL)
+//    {
+//        hasNotes=TRUE;
+//    }
+//    else
+//    {
+//        hasNotes=FALSE;
+//    }
+//    
+    
 }
 
 -(UIStatusBarStyle)preferredStatusBarStyle
@@ -57,6 +73,17 @@
     profileImageView.layer.masksToBounds = NO;
     profileImageView.clipsToBounds = YES;
     profileImageView.layer.cornerRadius=70;
+    
+    UILabel *navTitle = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 320, 40)];
+    navTitle.text = self.fullName;
+    navTitle.textColor = [UIColor whiteColor];
+    UIFont *navFont = [UIFont fontWithName:@"HelveticaNeue-Medium" size:28.0];
+    navTitle.font = navFont;
+    navTitle.textAlignment = NSTextAlignmentCenter;
+    [self.navigationController.navigationBar addSubview:navTitle];
+    
+    positionLabel.text = self.position;
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -66,6 +93,80 @@
 }
 
 
+#pragma mark - Buttons
+-(void)didTapPhone
+{
+    if([self.phoneNumbers count]==0)
+    {
+        UIAlertView *noPhone = [[UIAlertView alloc] initWithTitle:@"Error!" message:@"You don't have a phone number for this person." delegate:self cancelButtonTitle:@"Dismiss" otherButtonTitles:nil, nil];
+        [noPhone show];
+    }
+    
+    else if([self.phoneNumbers count] == 1)
+    {
+        NSString *phoneNo = [self.phoneNumbers objectAtIndex:0];
+        [self makeCall:phoneNo];
+    }
+    
+    else if([self.phoneNumbers count] > 1)
+    {
+        phonePicker = [[UIActionSheet alloc] initWithTitle:@"Which number?" delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles:nil, nil];
+        
+        for(NSString *number in self.phoneNumbers)
+            [phonePicker addButtonWithTitle:number];
+        
+        [phonePicker showInView:self.view];
+    }
+    
+    
+}
+
+-(void)didTapLinkedIn
+{
+    if(self.linkedInID != NULL)
+    {
+        NSString *rawURL = [NSString stringWithFormat:@"http://www.linkedin.com/profile/view?id=%@", self.linkedInID];
+        
+        NSURL *url = [NSURL URLWithString:rawURL];
+        
+        [[UIApplication sharedApplication] openURL:url];
+    }
+    
+    else
+    {
+        UIAlertView *noIDAlert =  [[UIAlertView alloc] initWithTitle:@"Error!" message:@"This person does not have a LinkedIn account set." delegate:self cancelButtonTitle:@"Dismiss" otherButtonTitles:nil, nil];
+        [noIDAlert show];
+    }
+}
+
+-(void)didTapShare
+{
+    NSArray *activityItems = @[@"hi", @"bro"];
+
+    UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:activityItems applicationActivities:nil];
+    activityVC.excludedActivityTypes = @[UIActivityTypePrint, UIActivityTypePostToWeibo];
+    [self presentViewController:activityVC animated:TRUE completion:nil];
+   
+}
+
+-(void)makeCall:(NSString*)phoneNo
+{
+    NSURL *phoneUrl = [NSURL URLWithString:[NSString  stringWithFormat:@"tel:%@",phoneNo]];
+    [[UIApplication sharedApplication] openURL:phoneUrl];
+}
+
+#pragma -mark Delegates/Data Sources
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    [alertView dismissWithClickedButtonIndex:1 animated:YES];
+}
+
+- (void)dismissWithClickedButtonIndex:(NSInteger)buttonIndex animated:(BOOL)animated
+{
+    NSString *phoneNo = [phonePicker buttonTitleAtIndex:buttonIndex];
+    [self makeCall:phoneNo];
+}
 
 /*
 #pragma mark - Navigation
