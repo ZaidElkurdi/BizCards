70a05a3cc3597a2a889b233c402a439989ac4f8b
Aryaman Sharda <aryamansharda@s00-0000-5245-res.dynamic.ucsd.edu>
2014-04-26 06:43:23 -0700

    before pull

--- Mitek Hackathon/AppDelegate.m
+++ Mitek Hackathon/AppDelegate.m
@@ -11,6 +11,8 @@
 
 @implementation AppDelegate
 
+
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
@@ -22,7 +24,7 @@
     
     return YES;
 }
-							
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
